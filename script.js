662703a1f6a0d3c666761383497947f3b18f6c3c
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-05-08 17:35:09 +0200

    integration function

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(event)
+{
+  let piString = '3.14159265358979323';
+
+  let pi = 4 * integralSimpson(function(x) { return 1 / (1+x*x);}, 0, 1, 250);
+
+  pi = pi.toFixed(17);
+  
+  console.log(pi);
+  console.log(piString);
+});
