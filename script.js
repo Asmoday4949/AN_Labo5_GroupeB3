83d544a0941be6293e0f6675c4a5419333ff5a41
kim.biloni <kim.biloni@he-arc.ch>
2018-05-09 11:30:52 +0200

    Ajout de la lib ploty et d'un graphe sans données

--- script.js
+++ script.js
@@ -9,4 +9,40 @@ document.addEventListener('DOMContentLoaded', function(event)
   console.log("Pi : " + piString);
   console.log("Evaluated Pi : " + pi);
   
+  displayPlot("graph");
 });
+
+function displayPlot(divPlot)
+{
+  let layout =
+  {
+    width: 1000,
+    height: 400,
+
+    xaxis:
+    {
+      range: [-4, 4],
+      autorange: false
+    },
+    yaxis:
+    {
+      range: [-1.1, 1.1],
+      autorange: false
+    },
+    hovermode: "closest"
+  };
+
+  let integratePiPlot =
+  {
+    name: 'f(x)',
+    /*x: points,
+    y: points,*/
+    type: 'scatter'
+  };
+
+  plotData = [];
+
+  //plotData.push();
+
+  Plotly.newPlot(divPlot, plotData, layout);
+}
